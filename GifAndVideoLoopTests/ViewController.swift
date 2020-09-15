44611871464eefa869d0a2270226ddb86fbd59d5
zakbarlow1995 <zak@stormideas.com>
2020-09-15 09:54:02 +0100

    Initial Commit

---
+++ GifAndVideoLoopTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GifAndVideoLoopTests
+//
+//  Created by Zak Barlow on 15/09/2020.
+//  Copyright © 2020 Zak Barlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
